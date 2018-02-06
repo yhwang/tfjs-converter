b0f22f149e136105eba9ff55eb2f67446a322a1c
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-02-06 11:06:47 -0800

    move the prototype code from deeplearn.js repo to tf-web

---
+++ demo/scavenger_classes.ts
@@ -0,0 +1,23 @@
+
+export const SCAVENGER_LABELS = [
+  'cookie',     'earphone',  'bed',      'burrito',
+  'speaker',    'printer',   'bicycle',  'mailbox',
+  'frypan',     'sunglass',  'wine',     'fish',
+  'baseball',   'knapsack',  'car',      'strawberry',
+  'book',       'cellphone', 'hotdog',   'wristwatch',
+  'tv',         'dog',       'succer',   'chinese lantern',
+  'icecream',   'display',   'shoe',     'mouth',
+  'laptop',     'piano',     'hammer',   'torch',
+  'broccoli',   'guitar',    'clock',    'hand',
+  'hat',        'sock',      'pizza',    'key',
+  'house',      'sushi',     'building', 'scissor',
+  'candle',     'umbrella',  'coat',     'door',
+  'envelope',   'bird',      'fries',    'keyboard',
+  'flower',     'donut',     'glove',    'bread',
+  'beer',       'lipstick',  'pen',      'cat',
+  'pants',      'orange',    'wallet',   'banana',
+  'taco',       'cabinet',   'lemon',    'sofa',
+  'microphone', 'trash can', 'lock',     'mug',
+  'mushroom',   'toilet',    'plate',    'camera',
+  'hamburger',  'tree',      'shirt',    'cassette'
+];
